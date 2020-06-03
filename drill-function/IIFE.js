3f337446850aa801589e21eb6045508b947f936a
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-06-04 02:12:07 +0600

    IIFE practiced

---
+++ drill-function/IIFE.js
@@ -0,0 +1,5 @@
+//IIFE means Immediately invoked expressions. it cover whole function in a first brackets and at the end give another first brackets. benefits, no need to call a function for run and also it provide high security.
+
+(function demo(){
+    console.log('my first IIFE test');
+})();
