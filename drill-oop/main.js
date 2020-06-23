c3a8b779893194193884dff467292cf36d71f31a
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-06-23 22:23:25 +0600

    object literals with methods

--- drill-oop/main.js
+++ drill-oop/main.js
@@ -1,1 +1,16 @@
-alert('done initialization')
+//object literals
+var rectangle = {
+  weight: 100,
+  height: 40,
+
+  draw: function () {
+    console.log("i am rectangle");
+    console.log("height" + this.height);
+    console.log("weight" + this.weight);
+  },
+};
+rectangle.draw();
+
+rectangle.weight = 300;
+
+rectangle.draw()
