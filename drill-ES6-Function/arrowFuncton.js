e880c2856fa0e7b1e4c874c475d5863fe9901213
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-07-14 13:02:27 +0600

    .

--- drill-ES6-Function/arrowFuncton.js
+++ drill-ES6-Function/arrowFuncton.js
@@ -9,3 +9,16 @@ console.log(num(33,33));
 var name = ()=>'akash';
 console.log(typeof name);
 console.log(name)
+
+//arrow function within object
+var obj={
+    name:'oaes kuruni',
+    print: function(){
+        setTimeout(() => {
+            console.log(this);
+            console.log(`hello ${this.name}`);
+        }, 1000);
+    }
+}
+
+obj.print()
---
+++ drill-wordCount/word.js
@@ -0,0 +1,14 @@
+//counting word within sentence
+
+var speech =
+  "tomorrow is my interview again i hope it will be better than before, inshaAllah";
+
+var count = 0;
+
+for (var i = 0; i < speech.length; i++) {
+  if (speech[i] == " ") {
+    count++;
+  }
+}
+count++;
+console.log(count);
