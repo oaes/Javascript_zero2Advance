176d8d256c48d98e08976e781b683cd7a991d64f
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-10-02 21:50:20 +0600

    practice

--- drill-array/find.js
+++ drill-array/find.js
@@ -1,8 +1,15 @@
 //find() methods check if the condition is match then it show it's output only it show first output of an array
 
-var num = [2,3,4,5,6,7]
+var num = [2, 3, 4, 5, 6, 7];
 
-var compare = num.find(num =>{
-    return num > 4
-})
+var compare = num.find((num) => {
+  return num > 4;
+});
 console.log(compare);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+let result = numbers.find((e) => {
+  return e > 4;
+});
+console.log(result);
